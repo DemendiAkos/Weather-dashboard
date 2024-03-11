f9450cac6bbf90b021fda6d8b956377cd19cbab6
Almos-Pal <75310455+Almos-Pal@users.noreply.github.com>
2024-03-11 21:32:39 +0100

    fix:env in right place correct url's

--- Frontend/Weather-Dashboard-Frontend/src/pages/Dashboard/DashboardPage.tsx
+++ Frontend/Weather-Dashboard-Frontend/src/pages/Dashboard/DashboardPage.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 
 function DashboardPage() {
+  const API_KEY = import.meta.env.VITE_API_KEY;
   const cityname = "Budapest";
-  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${proccess.env.API_KEY}&units=metric`;
-  const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityname}&appid=${proccess.env.API_KEY}&units=metric`;
+  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${API_KEY}&units=metric`;
+  const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityname}&appid=${API_KEY}&units=metric`;
 
   return <div></div>;
 }
