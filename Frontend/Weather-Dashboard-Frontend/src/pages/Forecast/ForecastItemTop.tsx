3ae3e1702f00dda22caac16c09f2098569a106c1
Bencuska^^ <bmbence72@gmail.com>
2024-04-10 08:57:02 +0200

    dosmthing3

--- Frontend/Weather-Dashboard-Frontend/src/pages/Forecast/ForecastItemTop.tsx
+++ Frontend/Weather-Dashboard-Frontend/src/pages/Forecast/ForecastItemTop.tsx
@@ -3,6 +3,8 @@ import { List } from "../../constants/forecastData";
 
 interface Props {
   item: List;
+  minTemp:number
+  minIcon:string
 }
 
 function ForecastItemTop(props: Props) {
@@ -27,7 +29,7 @@ function ForecastItemTop(props: Props) {
           src={`https://openweathermap.org/img/wn/${props.item.weather[0].icon}@2x.png`}
           alt={props.item.weather[0].description}
         />
-        <p>{props.item.main.temp_min}</p>
+        <p>{props.minTemp.toFixed()}</p>
         <p>{((props.item.wind.speed as number) * 3.6).toFixed(2)} <br /> km/h</p>
       </div>
     </div>
